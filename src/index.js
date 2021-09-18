7aa7816b1a0f91d45282fb694ea03954289bfd7b
Jon Ng <jonathanng15@gmail.com>
2021-09-18 01:57:31 -0700

    Add CSS to webpack config

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 `use strict`
 
+import './style.css';
+
 const homeTab = document.querySelector('#home-tab');
 const menuTab = document.querySelector('#menu-tab');
 const contactTab = document.querySelector('#contact-tab');
